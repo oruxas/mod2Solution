9d9fd9c6eaf5d3945696af20fcb2af41930efb8d
Evaldas <evaldaskal@gmail.com>
2016-09-24 23:00:21 +0300

    app.js, ng-controllers, ng-app

---
+++ app.js
@@ -0,0 +1,14 @@
+  (function () {
+     'use strict'; 
+
+
+  angular.module("ShoppingListCheckOff", [])
+    .controller("solutionController", function($scope){
+        
+
+
+    });
+
+}());
+  
+  
