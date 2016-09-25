f5cde7c839920f5cdacdcfef8369d6921b5be36b
Evaldas <evaldaskal@gmail.com>
2016-09-25 10:54:07 +0300

    toBuyArr

--- app.js
+++ app.js
@@ -23,6 +23,11 @@
     //Service Implementation
     function ShoppingListCheckOffService(){
         var service = this;
+
+        toBuyArr = [{name: "cookies", quantity: 10}, {name: "cookies", quantity: 11}, {name: "cookies", quantity: 12}, {name: "cookies", quantity: 13}, {name: "cookies", quantity: 14},
+        {name: "cookies", quantity: 15}, {name: "cookies", quantity: 16}, {name: "cookies", quantity: 17}, {name: "cookies", quantity: 18}, {name: "cookies", quantity: 19}];
+        boughtArr  = [];
+
     };
 
 }());
