da171ae5186b722913e9b2169ec42e4987fea401
Evaldas <evaldaskal@gmail.com>
2016-09-25 11:01:48 +0300

    fixing

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 
     //Controller Implementation
     function ToBuyShoppingController(ShoppingListCheckOffService) {
-        ShoppingListCheckOffService.getItmes();
+        ShoppingListCheckOffService.getItems();
 
     };
 
@@ -25,9 +25,12 @@
     function ShoppingListCheckOffService(){
         var service = this;
 
-        var toBuyArr = [{name: "cookies", quantity: 10}, {name: "cookies", quantity: 11}, {name: "cookies", quantity: 12}, {name: "cookies", quantity: 13}, {name: "cookies", quantity: 14},
+        var toBuyArr = [];
+        var boughtArr  = [];
+
+        toBuyArr = [{name: "cookies", quantity: 10}, {name: "cookies", quantity: 11}, {name: "cookies", quantity: 12}, {name: "cookies", quantity: 13}, {name: "cookies", quantity: 14},
         {name: "cookies", quantity: 15}, {name: "cookies", quantity: 16}, {name: "cookies", quantity: 17}, {name: "cookies", quantity: 18}, {name: "cookies", quantity: 19}];
-        boughtArr  = [];
+        
 
         service.getItems = function(){
             return toBuyArr;
