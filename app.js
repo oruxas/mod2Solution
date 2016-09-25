53fa5f2098f5dac0e3c7750ffed9a3fe8a0991a0
Evaldas <evaldaskal@gmail.com>
2016-09-25 14:39:23 +0300

    no unnecessary code

--- app.js
+++ app.js
@@ -11,59 +11,28 @@ ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyShoppingController(ShoppingListCheckOffService) {
     var toBuy = this;
     toBuy.items = ShoppingListCheckOffService.getItems();
-//   var itemAdder = this;
 
-//   itemAdder.itemName = "";
-//   itemAdder.itemQuantity = "";
-
-//   itemAdder.addItem = function () {
-//     ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
-//   }
 }
 
 
 AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtShoppingController(ShoppingListCheckOffServicee) {
-//   var showList = this;
 
-//   showList.items = ShoppingListService.getItems();
-
-//   showList.removeItem = function (itemIndex) {
-//     ShoppingListService.removeItem(itemIndex);
-//   };
 }
 
 
 function ShoppingListCheckOffService() {
   var service = this;
 
-  var toBuyArr = [];
+  var items = [];
 
-  toBuyArr = [{name: "cookies", quantity: 10}, {name: "cookies", quantity: 11}, {name: "cookies", quantity: 12}, {name: "cookies", quantity: 13}, {name: "cookies", quantity: 14},
+  items = [{name: "cookies", quantity: 10}, {name: "cookies", quantity: 11}, {name: "cookies", quantity: 12}, {name: "cookies", quantity: 13}, {name: "cookies", quantity: 14},
   {name: "cookies", quantity: 15}, {name: "cookies", quantity: 16}, {name: "cookies", quantity: 17}, {name: "cookies", quantity: 18}, {name: "cookies", quantity: 19}];
 
     service.getItems = function(){
-        return toBuyArr;
+        return items;
     }
 
-  // List of shopping items
-//   var items = [];
-
-//   service.addItem = function (itemName, quantity) {
-//     var item = {
-//       name: itemName,
-//       quantity: quantity
-//     };
-//     items.push(item);
-//   };
-
-//   service.removeItem = function (itemIdex) {
-//     items.splice(itemIdex, 1);
-//   };
-
-//   service.getItems = function () {
-//     return items;
-//   };
 }
 
 })();
