5088cfa57795e4a17dc3c633d104b6bdcf1c607b
Evaldas <evaldaskal@gmail.com>
2016-09-25 13:42:39 +0300

    tests

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-        // toBuyArr = [{name: "cookies", quantity: 10}, {name: "cookies", quantity: 11}, {name: "cookies", quantity: 12}, {name: "cookies", quantity: 13}, {name: "cookies", quantity: 14},
-        // {name: "cookies", quantity: 15}, {name: "cookies", quantity: 16}, {name: "cookies", quantity: 17}, {name: "cookies", quantity: 18}, {name: "cookies", quantity: 19}];
         
 (function () {
 'use strict';
